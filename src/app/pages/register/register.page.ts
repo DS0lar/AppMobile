bbcb17ba8bee22bca2bef072fbf9852ff00d7230
Diego <127960201+DS0lar@users.noreply.github.com>
2023-12-01 04:05:11 -0300

    actualizacion

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,19 +1,4 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { AlertController } from '@ionic/angular';
